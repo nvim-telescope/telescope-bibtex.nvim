95a6dae5bbbb5fd30be052041f28f1e1b4a5b3a6
Noah Wahl <noah.ares@yahoo.de>
2021-02-20 11:08:40 +0100

    Add md format for cite and allow custom user cite formats

--- lua/telescope/_extensions/bibtex.lua
+++ lua/telescope/_extensions/bibtex.lua
@@ -14,6 +14,11 @@ local path = require('plenary.path')
 local putils = require('telescope.previewers.utils')
 
 local depth = 1
+local formats = {}
+formats['tex'] = "\\cite{%s}"
+formats['md'] = "@%s"
+local fallback_format = 'tex'
+local user_format = fallback_format
 
 local function end_of_entry(line, par_mismatch)
   local line_blank = line:gsub("%s", "")
@@ -83,11 +88,11 @@ local function bibtex_picker(opts)
     sorter = conf.generic_sorter(opts),
     attach_mappings = function(prompt_bufnr)
       actions._goto_file_selection:replace(function(_, _)
-        local entry = "\\cite{"..actions.get_selected_entry().value.."}"
+        local entry = string.format(formats[user_format], actions.get_selected_entry().value)
         actions.close(prompt_bufnr)
         vim.api.nvim_put({entry}, "", true, true)
         -- TODO: prettier insert mode? <16-01-21, @noahares> --
-        vim.api.nvim_feedkeys("a", "n", true)
+        vim.api.nvim_feedkeys("ea", "n", true)
       end)
       return true
     end,
@@ -96,7 +101,15 @@ end
 
 return telescope.register_extension {
   setup = function(ext_config)
-    depth = ext_config.depth
+    depth = ext_config.depth or depth
+    local custom_formats = ext_config.custom_formats or {}
+    for _, format in pairs(custom_formats) do
+      formats[format.id] = format.cite_marker
+    end
+    user_format = ext_config.format or fallback_format
+    if formats[user_format] == nil then
+      user_format = fallback_format
+    end
   end,
   exports = {
     bibtex = bibtex_picker
