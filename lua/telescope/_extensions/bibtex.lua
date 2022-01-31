5e98ebe101989c606d64828b7e9942347cdb44bb
lambtho12 <github@thl.ovh>
2022-01-31 14:41:52 +0100

    Fix style and unnecessary global

--- lua/telescope/_extensions/bibtex.lua
+++ lua/telescope/_extensions/bibtex.lua
@@ -1,7 +1,7 @@
-local has_telescope, telescope = pcall(require, "telescope")
+local has_telescope, telescope = pcall(require, 'telescope')
 
 if not has_telescope then
-  error("This plugin requires telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)")
+  error('This plugin requires telescope.nvim (https://github.com/nvim-telescope/telescope.nvim)')
 end
 
 local finders = require('telescope.finders')
@@ -17,10 +17,10 @@ local loop = vim.loop
 
 local depth = 1
 local formats = {}
-formats['tex'] = "\\cite{%s}"
-formats['md'] = "@%s"
-formats['markdown'] = "@%s"
-formats['plain'] = "%s"
+formats['tex'] = '\\cite{%s}'
+formats['md'] = '@%s'
+formats['markdown'] = '@%s'
+formats['plain'] = '%s'
 local fallback_format = 'plain'
 local use_auto_format = false
 local user_format = ''
@@ -39,18 +39,22 @@ local function table_contains(table, element)
 end
 
 local function getBibFiles(dir)
-  scan.scan_dir(dir, { depth = depth, search_pattern = '.*%.bib', on_insert = function(file)
-    table.insert(files, {name = file, mtime = 0, entries = {}})
-  end })
+  scan.scan_dir(dir, {
+    depth = depth,
+    search_pattern = '.*%.bib',
+    on_insert = function(file)
+      table.insert(files, { name = file, mtime = 0, entries = {} })
+    end,
+  })
 end
 
 local function initFiles()
-  for _,file in pairs(user_files) do
+  for _, file in pairs(user_files) do
     local p = path:new(file)
     if p:is_dir() then
       getBibFiles(file)
     elseif p:is_file() then
-      table.insert(files, {name = file, mtime = 0, entries = {} })
+      table.insert(files, { name = file, mtime = 0, entries = {} })
     end
   end
   getBibFiles('.')
@@ -61,9 +65,11 @@ local function read_file(file)
   local contents = {}
   local search_relevants = {}
   local p = path:new(file)
-  if not p:exists() then return {} end
+  if not p:exists() then
+    return {}
+  end
   local data = p:read()
-  data = data:gsub("\r", "")
+  data = data:gsub('\r', '')
   local entries = {}
   local raw_entry = ''
   while true do
@@ -74,23 +80,25 @@ local function read_file(file)
     table.insert(entries, raw_entry)
     data = data:sub(#raw_entry + 2)
   end
-  for _,entry in pairs(entries) do
-    local label = entry:match("{%s*[^{},~#%\\]+,\n")
-    if (label) then
-      label = vim.trim(label:gsub("\n",""):sub(2, -2))
-      local content = vim.split(entry, "\n")
+  for _, entry in pairs(entries) do
+    local label = entry:match('{%s*[^{},~#%\\]+,\n')
+    if label then
+      label = vim.trim(label:gsub('\n', ''):sub(2, -2))
+      local content = vim.split(entry, '\n')
       table.insert(labels, label)
       contents[label] = content
       if table_contains(search_keys, [[label]]) then
         search_relevants[label]['label'] = label
       end
       search_relevants[label] = {}
-      for _,key in pairs(search_keys) do
+      for _, key in pairs(search_keys) do
         local match_base = '%f[%w]' .. key
-        local s = entry:match(match_base .. '%s*=%s*%b{}') or entry:match(match_base .. '%s*=%s*%b""') or entry:match(match_base .. '%s*=%s*%d+')
+        local s = entry:match(match_base .. '%s*=%s*%b{}')
+          or entry:match(match_base .. '%s*=%s*%b""')
+          or entry:match(match_base .. '%s*=%s*%d+')
         if s ~= nil then
           s = s:match('%b{}') or s:match('%b""') or s:match('%d+')
-          s = s:gsub('["{}\n]', ""):gsub('%s%s+', ' ')
+          s = s:gsub('["{}\n]', ''):gsub('%s%s+', ' ')
           search_relevants[label][key] = vim.trim(s)
         end
       end
@@ -120,18 +128,18 @@ local function setup_picker()
     files_initialized = true
   end
   local results = {}
-  for _,file in pairs(files) do
+  for _, file in pairs(files) do
     local mtime = loop.fs_stat(file.name).mtime.sec
     if mtime ~= file.mtime then
       file.entries = {}
       local result, content, search_relevants = read_file(file.name)
-      for _,entry in pairs(result) do
-	table.insert(results, { name = entry, content = content[entry], search_keys = search_relevants[entry] })
-	table.insert(file.entries, { name = entry, content = content[entry], search_keys = search_relevants[entry] })
+      for _, entry in pairs(result) do
+        table.insert(results, { name = entry, content = content[entry], search_keys = search_relevants[entry] })
+        table.insert(file.entries, { name = entry, content = content[entry], search_keys = search_relevants[entry] })
       end
       file.mtime = mtime
     else
-      for _,entry in pairs(file.entries) do
+      for _, entry in pairs(file.entries) do
         table.insert(results, entry)
       end
     end
@@ -155,12 +163,11 @@ end
 
 local function bibtex_picker(opts)
   opts = opts or {}
-  local mode = vim.api.nvim_get_mode().mode
   local format_string = parse_format_string(opts)
   local results = setup_picker()
   pickers.new(opts, {
     prompt_title = 'Bibtex References',
-    finder = finders.new_table {
+    finder = finders.new_table({
       results = results,
       entry_maker = function(line)
         local display_string, search_string = formatDisplay(line.search_keys)
@@ -180,13 +187,13 @@ local function bibtex_picker(opts)
             putils.highlighter(bufnr, 'bib')
           end,
         }
-      end
-    },
+      end,
+    }),
     previewer = previewers.display_content.new(opts),
     sorter = conf.generic_sorter(opts),
     attach_mappings = function(_, map)
       actions.select_default:replace(key_append(format_string))
-      map("i", "<c-e>", entry_append)
+      map('i', '<c-e>', entry_append)
       return true
     end,
   }):find()
@@ -194,13 +201,14 @@ end
 
 key_append = function(format_string)
   return function(prompt_bufnr)
+    local mode = vim.api.nvim_get_mode().mode
     local entry = string.format(format_string, action_state.get_selected_entry().id.name)
     actions.close(prompt_bufnr)
-    if mode == "i" then
-      vim.api.nvim_put({entry}, "", false, true)
-      vim.api.nvim_feedkeys("a", "n", true)
+    if mode == 'i' then
+      vim.api.nvim_put({ entry }, '', false, true)
+      vim.api.nvim_feedkeys('a', 'n', true)
     else
-      vim.api.nvim_put({entry}, "", true, true)
+      vim.api.nvim_put({ entry }, '', true, true)
     end
   end
 end
@@ -208,15 +216,16 @@ end
 entry_append = function(prompt_bufnr)
   local entry = action_state.get_selected_entry().id.content
   actions.close(prompt_bufnr)
-  if mode == "i" then
-    vim.api.nvim_put(entry, "", false, true)
-    vim.api.nvim_feedkeys("a", "n", true)
+  local mode = vim.api.nvim_get_mode().mode
+  if mode == 'i' then
+    vim.api.nvim_put(entry, '', false, true)
+    vim.api.nvim_feedkeys('a', 'n', true)
   else
-    vim.api.nvim_put(entry, "", true, true)
+    vim.api.nvim_put(entry, '', true, true)
   end
 end
 
-return telescope.register_extension {
+return telescope.register_extension({
   setup = function(ext_config)
     depth = ext_config.depth or depth
     local custom_formats = ext_config.custom_formats or {}
@@ -235,4 +244,4 @@ return telescope.register_extension {
   exports = {
     bibtex = bibtex_picker,
   },
-}
+})
